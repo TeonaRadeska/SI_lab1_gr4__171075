d7c1efdf85086006f8fe08cf7f38afdf6ffef9a4
Teona Radeska <teona.radeska@yahoo.com>
2020-04-10 20:42:14 +0200

    point class finished

--- Point.java
+++ Point.java
@@ -6,15 +6,64 @@ class Point {
 	//TODO add new variable
 
 	//TODO constructor
-
+    public Tocka(int x, int y) {
+    this.x=x;
+    this.y=y;
+  }
 	//TODO setters and getters
 
+public String getId() {
+
+		return id;
+
+	}
+
+	public void setId(String id) {
+
+		this.id = id;
+
+	}
+
+	public double getX() {
+
+		return x;
+
+	}
+
+	public void setX(double x) {
+
+		this.x = x;
+
+	}
+
+	public double getY() {
+
+		return y;
+
+	}
+
+
+
+	public void setY(double y) {
+
+		this.y = y;
+
+	}
 	public void move (char xDirection, char yDirection) {
 		//TODO
+		if(xDirection=='R')
+            x=x+1;
+        if(xDirection=='L')
+            x=x-1;
+        if(yDirection=='U')
+            y=y+1;
+        if(yDirection=='D')
+            y=y-1;
 	}
 
 	public void draw () {
 		//TODO
+		System.out.println(getX()+","+ getY());
 	}
 
 
